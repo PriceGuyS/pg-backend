9e73bc75e3ac1859809f266f634921b07d7d492d
DavisFrench <davisdavis27@gmail.com>
2017-09-24 12:06:21 -0400

    file of queries iterated through to get results. results are parsed for certain attribs and are stored into a list. This list is currently 'too big' (300 results), Might need to import directly from the script. If you are altering the queries realize that the specified categoryId is for videogames in an attempt to skip over tshirts, soundtracks, etc. I specify the meaning of the attribs I store in returnStructure. https://stackoverflow.com/questions/19336901/categoryid-does-not-seem-to-work-in-finditemsadvanced according to this category id depends on country but we're only dealing with US

--- ebay.py
+++ ebay.py
@@ -5,30 +5,40 @@ from ebaysdk.finding import Connection
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute
 
-def main(query, page):
+def main():
     try:
-        pages = {}
-        for i in range(int(page)):
-            api = Connection(appid="DavisFre-PriceGuy-PRD-d5d74fc8f-5a07a211", config_file=None)
-            response = api.execute('findItemsAdvanced', {'keywords': '{}'.format(query), # link below has more arguments for itemfilter
-                                                        "PaginationInput": { # does not work for w/e reason
-                                                        # "EntriesPerPage": 50,
-                                                        "PageNumber": (i+1)
-                                                        }
-                                                        }) # https://wiki.python.org/moin/UsingAssertionsEffectively
-                                                        # https://github.com/timotheus/ebaysdk-python/blob/master/samples/finding.py shows format
+        with open("n64list",'rU') as f: #n64 list contains list of queries that are iterated through
+            results = []
+            for query in f:
+                api = Connection(appid="DavisFre-PriceGuy-PRD-d5d74fc8f-5a07a211", config_file=None)
+                response = api.execute('findItemsAdvanced', {'keywords': '{}'.format(query), # link below has more arguments for itemfilter
+                                                            "PaginationInput": { # does not work for w/e reason
+                                                            # "EntriesPerPage": 50,
+                                                            "PageNumber": 1 # (i+1)
+                                                            },
+                                                            "categoryId": [
+                                                                '139973' # this is for videogames
+                                                            ]
+                                                            }) # https://wiki.python.org/moin/UsingAssertionsEffectively
+                                                            # https://github.com/timotheus/ebaysdk-python/blob/master/samples/finding.py shows format
 
-            assert(response.reply.ack == 'Success') # if not a 200 aka "success"
-            assert(type(response.reply.timestamp) == datetime.datetime) # assert is used for error checking
-            assert(type(response.reply.searchResult.item) == list) # see if result is a list (replace with string to see assert work)
-            assert(type(response.dict()) == dict) # makes sure response is of type dict else error?
-            # item = response.dict()
-            pages[i+1]=response.dict()
+                assert(response.reply.ack == 'Success') # if not a 200 aka "success"
+                assert(type(response.reply.timestamp) == datetime.datetime) # assert is used for error checking
+                # assert(type(response.reply.searchResult.item) == list) # errors out when no results returned
+                assert(type(response.dict()) == dict) # makes sure response is of type dict else error?
+                item = response.dict()
 
-        for i,listing in enumerate(pages[1]['searchResult']['item']):
-            print "listing {}: ".format(i)+str(listing)+"\n"
-        for i,listing in enumerate(pages[3]['searchResult']['item']):
-            print "listing {}: ".format(i)+str(listing)+"\n"
+                if item['paginationOutput']['totalPages'] == '0': # detects if 0 results returned, 0 also a string
+                    print "Search has no results"
+                else:
+                    for listing in item['searchResult']['item']:
+                        result = []
+                        result.extend((query, listing['title'], listing['sellingStatus']['convertedCurrentPrice']['value'], listing['sellingStatus']['convertedCurrentPrice']['_currencyId'], listing['condition']['conditionDisplayName'], listing['primaryCategory']['categoryName'], listing['country'], listing['shippingInfo']['shipToLocations']))
+                        results.append(result)
+                        print result
+
+            for i, line in results:
+                print "listing {}: ".format(i)+str(line)+"\n" #too many values to unpack should be 300
 
     except ConnectionError as e:
         print(e)
@@ -36,4 +46,4 @@ def main(query, page):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    main()
