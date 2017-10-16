73aea03b8bfcc91ddc0e5628ca2885558e1f3e19
zshumate <shumatezack@gmail.com>
2017-10-15 20:03:35 -0400

    testing newegg code, not working

---
+++ newegg/newegg.py
@@ -0,0 +1,21 @@
+import requests
+
+URL = "http://www.ows.newegg.com/Search.egg/Advanced/application/x-www-form-urlencoded"
+PARAMS = {
+        	"PageNumber": 1,
+        	"BrandId": -1,
+        	"NValue": "",
+        	"StoreDepaId": -1,
+        	"NodeId": -1,
+        	"Keyword": "Super Mario 64",
+        	"IsSubCategorySearch": False,
+        	"SubCategoryId": -1,
+        	"Sort": "FEATURED",
+        	"CategoryId": -1,
+        	"IsUPCCodeSearch": False
+        }
+
+r = requests.post(url=URL, data=PARAMS)
+
+print(r.json)
+print(r.text)
