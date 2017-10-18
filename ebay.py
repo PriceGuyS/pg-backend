d1456715fc64f538d6dddf18afa7b7f9757b5ebb
DavisFrench <davisdavis27@gmail.com>
2017-10-18 18:09:59 -0400

    my three files have been connected. Declare the table name and json file names in ebay.api in the corresponding variables, which are passed to the other scripts that need them. A delay is place between table creation and loading data because w/o it the table doesnt get created before loading happens. This was tested on a much smaller dataset, but I'm pretty sure table creation time is the same no matter the dataset. 10s on the wait might be high, but it works.

--- ebay.py
+++ ebay.py
@@ -1,34 +1,32 @@
 import datetime
 import json
+import time
 from ebaysdk.exception import ConnectionError
 from ebaysdk.finding import Connection
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute
+from listingsCreateTable import createTable
+from listingsLoadData import loadData
 
 def main():
-    try:
+    try: # insert a cron job?
         with open("n64list",'rU') as f: #n64 list contains list of queries that are iterated through
             results = []
             for query in f:
                 api = Connection(appid="DavisFre-PriceGuy-PRD-d5d74fc8f-5a07a211", config_file=None)
                 response = api.execute('findItemsAdvanced', {'keywords': '{}'.format(query), # link below has more arguments for itemfilter
                                                             "PaginationInput": { # does not work for w/e reason
-                                                            # "EntriesPerPage": 50,
                                                                 "PageNumber": 1 # page works but results doesnt
                                                             },
-                                                            "itemFilter": [ # https://developer.ebay.com/devzone/finding/callref/types/ListingInfo.html
-                                                            # https://developer.ebay.com/devzone/finding/callref/types/ItemFilterType.html
-                                                            # theres listinginfo and listing, not sure which is right and I can't get either to filter properly
-
+                                                            "itemFilter": [
                                                                     # {'name': 'ListingType', 'value': 'AuctionWithBIN'}, # not sure how to get both
                                                                     {'name': 'ListingType', 'value': 'FixedPrice'} # allegedly you can just give one name two values but it didnt seem to work
-
+                                                                    # https://developer.ebay.com/devzone/finding/callref/types/ItemFilterType.html
                                                             ],
                                                             "categoryId": [
                                                                 '139973' # this is for videogames
                                                             ]
-                                                            }) # https://wiki.python.org/moin/UsingAssertionsEffectively
-                                                            # https://github.com/timotheus/ebaysdk-python/blob/master/samples/finding.py shows format
+                                                            })
 
                 assert(response.reply.ack == 'Success') # if not a 200 aka "success"
                 assert(type(response.reply.timestamp) == datetime.datetime) # assert is used for error checking
@@ -39,7 +37,7 @@ def main():
                     print "Search has no results"
                 else:
                     for listing in item['searchResult']['item']:
-                        print listing['listingInfo']['buyItNowAvailable']
+                        print query
                         listingDict = {}
                         listingDict['id'] = listing['itemId']; listingDict['query'] = query.rstrip(); listingDict['title'] = listing['title']; listingDict['URL'] = listing['viewItemURL']
                         listingDict['price'] = listing['sellingStatus']['convertedCurrentPrice']['value']; listingDict['currency'] = listing['sellingStatus']['convertedCurrentPrice']['_currencyId']
@@ -47,8 +45,14 @@ def main():
                         listingDict['category'] = listing['primaryCategory']['categoryName']; listingDict['country'] = listing['country']; listingDict['shipsTo'] = listing['shippingInfo']['shipToLocations']
                         results.append(listingDict)
 
-        with open('jsonResults1.json', 'w') as w: # might wannna do a for loop and do appends?
+        jsonFile = 'jsonResults3.json'
+        tableName = 'testTable'
+        with open(jsonFile, 'w') as w: # might wannna do a for loop and do appends?
             json.dump(results, w, indent=4, sort_keys=True)
+
+        createTable(tableName)
+        time.sleep(10)
+        loadData(jsonFile, tableName)
 
 
     except ConnectionError as e:
--- listingsLoadData.py
+++ listingsLoadData.py
@@ -3,43 +3,44 @@ import boto3
 import json
 import decimal
 
-# http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.Python.html tutorial!
-mysession = boto3.session.Session(aws_access_key_id='ACCESS_KEY', aws_secret_access_key='SECRET_KEY') # this accesses the keys in the 'credentials' file in ~/.aws/
-#mysession = boto3.session.Session(profile_name="davis2") # for specifying user
-dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+def loadData(jsonFile, name):
+    # http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.Python.html tutorial!
+    mysession = boto3.session.Session(aws_access_key_id='ACCESS_KEY', aws_secret_access_key='SECRET_KEY') # this accesses the keys in the 'credentials' file in ~/.aws/
+    #mysession = boto3.session.Session(profile_name="davis2") # for specifying user
+    dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
 
-table = dynamodb.Table('Listings1')
+    table = dynamodb.Table(name)
 
-with open("jsonResults1.json") as json_file:
-    listings = json.load(json_file, parse_float = decimal.Decimal)
-    for listing in listings:
-        URL = listing['URL']
-        category = listing['category']
-        condition = listing['condition']
-        country = listing['country']
-        currency = listing['currency']
-        endTime = listing['endTime']
-        ID = listing['id'] # case not matching because I didn't realize id is a keyword
-        price = listing['price']
-        query = listing['query']
-        shipsTo = listing['shipsTo']
-        title = listing['title']
+    with open(jsonFile) as json_file:
+        listings = json.load(json_file, parse_float = decimal.Decimal)
+        for listing in listings:
+            URL = listing['URL']
+            category = listing['category']
+            condition = listing['condition']
+            country = listing['country']
+            currency = listing['currency']
+            endTime = listing['endTime']
+            ID = listing['id'] # case not matching because I didn't realize id is a keyword
+            price = listing['price']
+            query = listing['query']
+            shipsTo = listing['shipsTo']
+            title = listing['title']
 
 
-        print("Adding listing: ", title)
+            print("Adding listing: ", title)
 
-        table.put_item(
-           Item={
-               'URL': URL,
-               'category': category,
-               'condition': condition,
-               'country': country,
-               'currency': currency,
-               'endTime': endTime,
-               'id': ID,
-               'price': price,
-               'query': query,
-               'shipsTo': shipsTo,
-               'title': title
-            }
-        )
+            table.put_item(
+               Item={
+                   'URL': URL,
+                   'category': category,
+                   'condition': condition,
+                   'country': country,
+                   'currency': currency,
+                   'endTime': endTime,
+                   'id': ID,
+                   'price': price,
+                   'query': query,
+                   'shipsTo': shipsTo,
+                   'title': title
+                }
+            )
