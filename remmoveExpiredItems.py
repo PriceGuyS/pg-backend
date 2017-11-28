4011fd8a624cce111be9fafa857a6e852d26435e
zshumate <shumatezack@gmail.com>
2017-11-27 22:42:06 -0500

    start script to removed expired ebay listings

---
+++ remmoveExpiredItems.py
@@ -0,0 +1,14 @@
+from __future__ import print_function # Python 2/3 compatibility
+import boto3
+import json
+import decimal
+
+def removeItems(tableName):
+    # http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.Python.html tutorial!
+    mysession = boto3.session.Session(aws_access_key_id='ACCESS_KEY', aws_secret_access_key='SECRET_KEY') # this accesses the keys in the 'credentials' file in ~/.aws/
+    #mysession = boto3.session.Session(profile_name="davis2") # for specifying user
+    dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+
+    table = dynamodb.Table(tableName)
+
+    
