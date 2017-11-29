17d30d92210f0999b8d2064457e5ea4cb3bf0bd5
zshumate <shumatezack@gmail.com>
2017-11-29 15:06:20 -0500

    writing query for expired items to be deleted

--- removeExpiredItems.py
+++ removeExpiredItems.py
@@ -3,19 +3,20 @@ import boto3
 import json
 import decimal
 import datetime
+from boto3.dynamodb.conditions import Key, Attr
 
 def removeItems(tableName):
     # http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.Python.html tutorial!
     mysession = boto3.session.Session(aws_access_key_id='ACCESS_KEY', aws_secret_access_key='SECRET_KEY') # this accesses the keys in the 'credentials' file in ~/.aws/
     #mysession = boto3.session.Session(profile_name="davis2") # for specifying user
     dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
-    today = datetime.date()
+    today = str(datetime.date.today())
 
     table = dynamodb.Table(tableName)
 
     response = table.query(
-        KeyConditionExpression=Key('endTime') < today
+        KeyConditionExpression=Key('endTime').lt(today)
     )
 
     for i in response['Items']:
-        print i['id']
+        print(i['id'])
